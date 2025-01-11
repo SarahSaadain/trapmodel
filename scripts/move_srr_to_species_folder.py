ceb9de55f5e3847f26d0fbd659406eaeae03ba5b
SarahSaadain <89971132+SarahSaadain@users.noreply.github.com>
2025-01-11 17:11:24 +0100

    Update move_srr_to_species_folder.py

--- scripts/move_srr_to_species_folder.py
+++ scripts/move_srr_to_species_folder.py
@@ -3,21 +3,51 @@ import shutil
 import argparse
 import subprocess
 
-def create_directory_structure_and_move(source_files, target_files, source_folder, target_folder):
+def download_sra_files(sra_folder, sra_accession_list):
+
+    print("Downloading SRA files ...")
+
+    for sra_number in sra_accession_list:
+
+        print(f"Downloading {sra_number} ...")
+
+        sra_file = os.path.join(sra_folder,sra_number,sra_number+".sra")
+
+        if os.path.exists(sra_file):
+            print(f"File alredy downloaded: {sra_file}")
+            continue #with the next
+        
+        try:
+            
+            subprocess.run(["prefetch", sra_number, "-O", os.path.join(sra_folder)], check=True)
+            print(f"Downloaded {sra_file}")
+
+            if not os.path.exists(sra_file):
+                print(f"Error downloading {sra_number}")
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error downloading {sra_number}: {e}")
+
+        
+       
+def move_sra_and_convert(sra_accession_list: list, srna_filenames_list: list, sra_folder: str, raw_folder: str):
     """
     Move and rename files based on the provided lists, creating a directory structure.
 
     Args:
-        source_files (list): A list of source filenames.
-        target_files (list): A list of target filenames corresponding to the source filenames.
-        source_folder (str): Path to the source folder containing files to move.
-        target_folder (str): Path to the target folder where files will be moved.
+        sra_accession_list (list): A list of sra file accessions.
+        srna_filenames_list (list): A list of sRNA filenames corresponding to the sRNA filesnames.
+        sra_folder (str): Path to the sra folder containing files to move.
+        raw_folder (str): Path to the raw target folder where files will be moved.
     """
-    for source_file in source_files:
+
+    print("Copy and convert SRA files to species raw folders ...")
+
+    for sra_number in sra_accession_list:
         # Find the target file containing the source file name
-        target_file = next((t for t in target_files if source_file in t), None)
+        target_file = next((t for t in srna_filenames_list if sra_number in t), None)
         if not target_file:
-            print(f"No matching target file found for source file: {source_file}")
+            print(f"No matching target file found for source file: {sra_number}")
             continue
 
         # Parse the target file name to get folder and subfolder
@@ -30,18 +60,22 @@ def create_directory_structure_and_move(source_files, target_files, source_folde
         subfolder = parts[1]  # Second part as subfolder
 
         # Create folder and subfolder in the target directory
-        target_path = os.path.join(target_folder, folder, "sRNA", subfolder)
+        target_path = os.path.join(raw_folder, folder, "sRNA", subfolder)
         os.makedirs(target_path, exist_ok=True)
 
         # Move and rename the file
-        source_path = os.path.join(source_folder,source_file,source_file+".sra")
-        if not os.path.isfile(source_path):
-            print(f"Source file not found: {source_path}")
-            continue
-
+        source_path = os.path.join(sra_folder,sra_number,sra_number+".sra")
         new_file_path = os.path.join(target_path, target_file+".sra")
-        shutil.copy(source_path, new_file_path)
-        print(f"Copied {source_path} to {new_file_path}")
+
+        if not os.path.exists(new_file_path):
+            if not os.path.isfile(source_path):
+                print(f"Source file not found: {source_path}")
+                continue
+
+            shutil.copy(source_path, new_file_path)
+            print(f"Copied {source_path} to {new_file_path}")
+        else:
+            print(f"SRA {sra_number} file alredy copied -> SKIP")
 
         # Execute command on the target file
         try:
@@ -50,6 +84,8 @@ def create_directory_structure_and_move(source_files, target_files, source_folde
             if not os.path.exists(fastq_file_path):
                 subprocess.run(["fasterq-dump", "--split-files", new_file_path, "--outdir", target_path], check=True)
                 print(f"Executed fasterq-dump for {target_file}.sra")
+            else: 
+                print(f"SRA {sra_number} file alredy converted to FASTQ -> SKIP")
 
             # Run seqtk to convert FASTQ to FASTA
             fasta_file = os.path.join(target_path, f"{target_file}.fasta")
@@ -57,30 +93,33 @@ def create_directory_structure_and_move(source_files, target_files, source_folde
             if not os.path.exists(fasta_file):
                 subprocess.run(["seqtk", "seq", "-A", fastq_file_path], stdout=open(fasta_file, 'wb'), check=True)
                 print(f"Converted {target_file}.fastq to FASTA: {target_file}.fasta")
+            else:
+                print(f"SRA {sra_number} file alredy converted to FASTA -> SKIP")
         except subprocess.CalledProcessError as e:
-            print(f"Error executing fasterq-dump for {source_file}: {e}")
+            print(f"Error executing fasterq-dump for {sra_folder}: {e}")
 
 def main():
     parser = argparse.ArgumentParser(description="Move and rename files based on source and target lists.")
-    parser.add_argument("source_folder", type=str, help="Path to the source folder.")
-    parser.add_argument("target_folder", type=str, help="Path to the target folder.")
-    parser.add_argument("source_file_list", type=str, help="Path to the file containing source filenames.")
-    parser.add_argument("target_file_list", type=str, help="Path to the file containing target filenames.")
+    parser.add_argument("sra_folder", type=str, help="Path to the SRA folder.")
+    parser.add_argument("raw_folder", type=str, help="Path to the raw folder.")
+    parser.add_argument("sra_accession_list", type=str, help="Path to the file containing SRA accession list.")
+    parser.add_argument("srna_filenames_list", type=str, help="Path to the file containing sRNA filenames.")
 
     args = parser.parse_args()
 
     # Read source and target file lists
-    with open(args.source_file_list, "r") as f:
-        source_files = [line.strip() for line in f]
+    with open(args.sra_accession_list, "r") as f:
+        sra_accession_list = [line.strip() for line in f]
 
-    with open(args.target_file_list, "r") as f:
-        target_files = [line.strip() for line in f]
+    with open(args.srna_filenames_list, "r") as f:
+        srna_filenames_list = [line.strip() for line in f]
 
-    if len(source_files) != len(target_files):
+    if len(sra_accession_list) != len(srna_filenames_list):
         print("Error: The number of source and target filenames must be the same.")
         return
 
-    create_directory_structure_and_move(source_files, target_files, args.source_folder, args.target_folder)
+    download_sra_files(args.sra_folder, sra_accession_list)
+    move_sra_and_convert(sra_accession_list, srna_filenames_list, args.sra_folder, args.raw_folder)
 
 if __name__ == "__main__":
     main()
